e7655f43a856ca7f91c3bd3c85ebc99fb54cc0a9
Akash4590 <akashjaved4590@gmail.com>
2026-09-01 16:57:48 +0500

    added front changes

--- Frontend/src/pages/Shop.tsx
+++ Frontend/src/pages/Shop.tsx
@@ -63,7 +63,7 @@ const ShopPage = () => {
         setError("");
 
         const response = await fetch(
-          "http://localhost:5000/api/products"
+        `${import.meta.env.VITE_API_URL}/api/products`
         );
 
         if (!response.ok) {
