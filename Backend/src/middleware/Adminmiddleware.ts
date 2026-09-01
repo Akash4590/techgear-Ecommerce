47d1a36864fe9851a106bfe79bcc2a588ea3f79d
Akash4590 <akashjaved4590@gmail.com>
2026-09-01 16:15:40 +0500

    error solve

--- Backend/src/middleware/Adminmiddleware.ts
+++ Backend/src/middleware/Adminmiddleware.ts
@@ -1,7 +1,7 @@
 import type { Response, NextFunction } from "express";
 import type { AuthRequest } from "./authMiddleware.js";
 
-// Hamesha authMiddleware ke BAAD use karna hai, taake req.userRole set ho chuka ho
+
 export const adminMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
   if (req.userRole !== "admin") {
     return res.status(403).json({
